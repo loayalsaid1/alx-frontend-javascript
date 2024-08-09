7acaf83f96e09c9cab62a570959a0ffc38bbe2ec
Loay Al-Said <l.programming555@gmail.com>
2024-08-09 04:28:46 +0300

    2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,49 @@
+export default class HolbertonCourse {
+  static _checkType(value, type, name) {
+    if (typeof value !== type && type !== 'array') { // eslint-disable-line valid-typeof
+      throw new TypeError(`${name} must be a ${type}`);
+    }
+
+    if (type === 'array' && !(value instanceof Array)) {
+      throw new TypeError(`${name} must be a ${type}`);
+    }
+
+    return value;
+  }
+
+  constructor(name, length, students) {
+    this._name = HolbertonCourse._checkType(name, 'string', 'Name');
+    this._length = HolbertonCourse._checkType(length, 'number', 'Length');
+    this._students = HolbertonCourse._checkType(students, 'object', 'Students');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = HolbertonCourse._checkType(value, 'string', 'Name');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = HolbertonCourse._checkType(value, 'number', 'Length');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._students = HolbertonCourse._checkType(value, 'Array', 'Students');
+
+    value.forEach((element) => {
+      if (!(element instanceof String)) {
+        throw new TypeError('Students must be an array of strings');
+      }
+    });
+  }
+}
